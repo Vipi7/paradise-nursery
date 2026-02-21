fd5ac36c767a37c98c18d7d59bdcb9a73c5b250e
Vipin prajapati <121173675+Vipi7@users.noreply.github.com>
2026-02-21 19:58:09 +0530

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,1 +1,9 @@
-
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>We provide healthy and beautiful plants for your home.</p>
+    </div>
+  );
+}
+export default AboutUs;
