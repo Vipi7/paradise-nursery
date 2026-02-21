7f7d2c406d821f0b083a34354b2c471e0df9519e
Vipin prajapati <121173675+Vipi7@users.noreply.github.com>
2026-02-21 20:21:26 +0530

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,29 @@
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/CartSlice";
+
+const plants = [
+  { id: 1, name: "Snake Plant", price: 15 },
+  { id: 2, name: "Aloe Vera", price: 10 },
+  { id: 3, name: "Peace Lily", price: 20 }
+];
+
 function ProductList() {
-  return <h2>Product List</h2>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Plant Collection</h2>
+      {plants.map(plant => (
+        <div key={plant.id}>
+          <h3>{plant.name}</h3>
+          <p>${plant.price}</p>
+          <button onClick={() => dispatch(addItem(plant))}>
+            Add to Cart
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
+
 export default ProductList;
