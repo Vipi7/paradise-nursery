01b656e1e82562c81321ade4f4db3fec891eb965
Vipin prajapati <121173675+Vipi7@users.noreply.github.com>
2026-02-21 20:45:18 +0530

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,21 @@
 import { useDispatch } from "react-redux";
 import { addItem } from "../redux/CartSlice";
 
-const plants = [
-  { id: 1, name: "Snake Plant", price: 15 },
-  { id: 2, name: "Aloe Vera", price: 10 },
-  { id: 3, name: "Peace Lily", price: 20 }
+const plantsArray = [
+  {
+    category: "Indoor Plants",
+    plants: [
+      { id: 1, name: "Snake Plant", price: 15, image: "snake.jpg" },
+      { id: 2, name: "Aloe Vera", price: 10, image: "aloe.jpg" },
+    ],
+  },
+  {
+    category: "Outdoor Plants",
+    plants: [
+      { id: 3, name: "Rose Plant", price: 20, image: "rose.jpg" },
+      { id: 4, name: "Tulsi", price: 12, image: "tulsi.jpg" },
+    ],
+  },
 ];
 
 function ProductList() {
@@ -12,14 +23,27 @@ function ProductList() {
 
   return (
     <div>
-      <h2>Plant Collection</h2>
-      {plants.map(plant => (
-        <div key={plant.id}>
-          <h3>{plant.name}</h3>
-          <p>${plant.price}</p>
-          <button onClick={() => dispatch(addItem(plant))}>
-            Add to Cart
-          </button>
+      <h2>Our Plants</h2>
+
+      {plantsArray.map((group) => (
+        <div key={group.category}>
+          <h3>{group.category}</h3>
+
+          {group.plants.map((plant) => (
+            <div key={plant.id}>
+              <img
+                src={plant.image}
+                alt={plant.name}
+                width="100"
+              />
+              <h4>{plant.name}</h4>
+              <p>${plant.price}</p>
+
+              <button onClick={() => dispatch(addItem(plant))}>
+                Add to Cart
+              </button>
+            </div>
+          ))}
         </div>
       ))}
     </div>
